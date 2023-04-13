ac286bc599aca4c660f67b08e0b8d8e2db686b50
Ola <scubesept@gmail.com>
2023-04-13 08:21:46 -0700

    fix up

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+int _putchar(char c);/*prototype _putchar*/
+char *create_array(unsigned int size, char c);/*prototype function that creates an array of chars, and initializes it with a specific char.*/
+char *_strdup(char *str);/*function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.*/
+char *str_concat(char *s1, char *s2);/*prototype function that concatenates two strings*/
+int **alloc_grid(int width, int height);/*prototype function that returns a pointer to a 2 dimensional array of integers.*/
+void free_grid(int **grid, int height);/*prototype function that frees a 2 dimensional grid previously created by your alloc_grid function.*/
+char *argstostr(int ac, char **av);/*prototype function that concatenates all the arguments of your program.*/
+
+#endif /* MAIN_H */
